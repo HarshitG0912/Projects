db825b28e8bf1d568ff01c8ec78750526ea3ef8b
HarshitG0912 <hkgupta744@gmail.com>
2026-05-17 22:23:01 +0530

    Add files via upload

---
+++ WAP to input 2 floating point numbers & print their average.py
@@ -0,0 +1,8 @@
+# let two number be a and b 
+a= float(input("please put the value of a:" ))
+b=float(input("please put the value of b:" ))
+avg=float((a+b)/2)
+
+print("average of given two numbers is:",avg)
+
+
