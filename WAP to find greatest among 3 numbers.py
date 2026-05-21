5531ea53df8a553b9b82962224ba9758881000f4
HarshitG0912 <hkgupta744@gmail.com>
2026-05-21 12:27:17 +0530

    Delete WAP to find greatest among 3 numbers.py

--- WAP to find greatest among 3 numbers.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter a= "))
-b=int(input("enter b= "))
-c=int(input("enter c= "))
-if(a>=b and a>=c):
-    print("a is the greatest.")
-elif(b>=c):
-    print("b is greatest.")
-else:
-    print("c is greatest.")
